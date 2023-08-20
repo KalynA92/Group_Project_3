1d09dc91e932feeeabf8164fb959125dc00fb41a
Kalyn <kalynaguiar@hotmail.com>
2023-08-20 12:31:42 -0400

    cleaning code to plot correctly

--- plots/plot_Kalyn.js
+++ plots/plot_Kalyn.js
@@ -2,12 +2,39 @@
  ********* Kalyn's Plot ******************
  *****************************************/
  const kurl = "https://raw.githubusercontent.com/KalynA92/Group_Project_3/main/Resources/cityshape.js";
-
-// Promise Pending
-const dataPromise = d3.json(kurl);
-console.log("Data: ", dataPromise);
+ 
 
 // Fetch the JSON data and console log it
 d3.json(kurl).then(function(data) {
   console.log(data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
+
+
