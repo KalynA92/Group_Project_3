74cf4b7729d970cecf3309659e74820151a8c784
Kalyn <kalynaguiar@hotmail.com>
2023-08-20 12:37:56 -0400

    cleaning the data for plotting

--- plots/plot_Kalyn.js
+++ plots/plot_Kalyn.js
@@ -3,7 +3,6 @@
  *****************************************/
  const kurl = "https://raw.githubusercontent.com/KalynA92/Group_Project_3/main/Resources/cityshape.js";
  
-
 // Fetch the JSON data and console log it
 d3.json(kurl).then(function(data) {
   console.log(data);
