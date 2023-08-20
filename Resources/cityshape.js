bab00b09440a1f6904817a64191f9c7a2b985193
Kalyn <kalynaguiar@hotmail.com>
2023-08-20 10:41:43 -0400

    cleaning data for plotting

--- Resources/cityshape.js
+++ Resources/cityshape.js
@@ -1,9 +1,7 @@
-most_active_date = [
-  {"Shape":"fireball","Totals":11},
-  {"Shape":"circle","Totals":6},
-  {"Shape":"light","Totals":3},
-  {"Shape":"unknown","Totals":3},
-  {"Shape":"other","Totals":2},
-  {"Shape":"sphere","Totals":2},
-  {"Shape":"changing","Totals":1}
-]
+[{"Shape":"fireball","Totals":11},
+{"Shape":"circle","Totals":6},
+{"Shape":"light","Totals":3},
+{"Shape":"unknown","Totals":3},
+{"Shape":"other","Totals":2},
+{"Shape":"sphere","Totals":2},
+{"Shape":"changing","Totals":1}]
